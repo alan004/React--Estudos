8bab0ee938c04ef484335088bc8fff9bb94f20ec
alan04 <alandocarmoabilio@gmail.com>
2023-07-25 17:22:24 -0300

    fix: (banner) correção da imagem

--- alura-organo/src/componentes/banner/index.js
+++ alura-organo/src/componentes/banner/index.js
@@ -3,7 +3,7 @@ import "./banner.css";
 function Banner() {
   return (
     <header className="banner">
-      <img src="/imagens/banner.png" alt="Imagem do banner principal" />
+      <img src="/imagens/Banner.png" alt="Imagem do banner principal" />
     </header>
   );
 }
