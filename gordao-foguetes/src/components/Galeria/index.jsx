dd38823e9144f13010d38fbc6be3bd8fee609b3f
alan04 <alandocarmoabilio@gmail.com>
2023-08-03 19:45:06 -0300

    style: galeria imagens

--- gordao-foguetes/src/components/Galeria/index.jsx
+++ gordao-foguetes/src/components/Galeria/index.jsx
@@ -1,19 +1,15 @@
 import Tags from "../Tags";
 import styles from './galeria.module.scss'
 import fotos from './fotos.json'
-import favorito from './favorito.png'
-import open from './open.png'
+import Cards from "./Cards";
+
 
 export default function Galeria(){
     return(
         <section className={styles.galeria}>
             <h2>Navegue pela galeria</h2>
             <Tags/>
-            <ul>
-                {fotos.map((foto)=>{
-                    return 'olá'
-                })}
-            </ul>
+            <Cards itens={fotos} style={styles} />
         </section>
     )
 }
