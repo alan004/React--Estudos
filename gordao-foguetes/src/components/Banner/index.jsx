d30de2b66ed9c86937210e05826868c2e769da7c
alan04 <alandocarmoabilio@gmail.com>
2023-08-03 20:41:37 -0300

    feat: seção fotos populares

--- gordao-foguetes/src/components/Banner/index.jsx
+++ gordao-foguetes/src/components/Banner/index.jsx
@@ -1,7 +1,7 @@
 export default function Banner(props) {
   return (
     <div className={props.css}>
-      <h1>A galeria mais completa do Sergio Sacani</h1>
+      <h1>Uma galeria completa do Sergio Sacani!</h1>
       <img src={props.imagem} alt="Imagem do Sergio Sacani" />
     </div>
   );
--- gordao-foguetes/src/pages/Homepage/index.jsx
+++ gordao-foguetes/src/pages/Homepage/index.jsx
@@ -3,6 +3,7 @@ import Banner from "../../components/Banner";
 import Cabecalho from "../../components/Cabecalho";
 import Galeria from "../../components/Galeria";
 import Menu from "../../components/Menu";
+import Populares from "../../components/Populares";
 import Rodape from "../../components/Rodape";
 import bannerSergio from './banner.png'
 import styles from './homepage.module.scss'
@@ -16,10 +17,11 @@ export default function Homepage(){
                 <Menu/>
                 <Banner css={styles.principal__imagem} imagem={bannerSergio}/>
             </section>
+            <div className={styles.galeria}>
+                <Galeria/>
+                <Populares/>
+            </div>
         </main>
-        <div className={styles.galeria}>
-            <Galeria/>
-        </div>
         <Rodape/>
         </>
     )
