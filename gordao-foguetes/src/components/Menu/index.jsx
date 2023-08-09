c821f5c12fd793a70c76caeda07cfb776a0edd21
alan04 <alandocarmoabilio@gmail.com>
2023-08-09 20:50:07 -0300

    style: menu responsivo

--- gordao-foguetes/src/components/Menu/index.jsx
+++ gordao-foguetes/src/components/Menu/index.jsx
@@ -1,15 +1,29 @@
-import icones from './icones.json'
-import Icone from './Icone';
+import icones from "./icones.json";
+import Icone from "./Icone";
+import {  AiOutlineMenu } from 'react-icons/ai'
 
 
-import styles from './menu.module.scss'
+import styles from "./menu.module.scss";
 
-export default function Menu(){
-    return(
-        <nav className={styles.menu}>
-            <ul className={styles.menu__lista}>
-            {icones.map(icone => (<Icone key={icone.id} icone={icone} style={styles} />))}
-            </ul>
-        </nav>
-    )
+export default function Menu() {
+  return (
+    <>
+      <nav className={styles.menu}>
+        <input
+          id="ativarMenu"
+          name="menu"
+          type="checkbox"
+          className={styles.menu__ativarCheckbox}
+        />
+      <label htmlFor="ativarMenu" className={styles.menu__ativarIMG}>
+          <AiOutlineMenu color="#c98cf1" size={30}></AiOutlineMenu>
+        </label>
+        <ul className={styles.menu__lista}>
+          {icones.map((icone) => (
+            <Icone key={icone.id} icone={icone} style={styles} />
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 }
