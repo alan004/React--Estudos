6c0cdd179232f8bcd7a9e5b8e66dbeaca347617c
alan04 <alandocarmoabilio@gmail.com>
2023-08-23 20:28:49 -0300

    feat: index and about page

--- creed-thoughts/src/pages/Home/index.jsx
+++ creed-thoughts/src/pages/Home/index.jsx
@@ -1,9 +1,18 @@
 import Banner from "components/Banner";
+import styles from "./Home.module.css"
+import posts from 'json/posts.json'
+import Post from "components/Post";
 
 export default function Home(){
     return (
         <>
-            <Banner />
+            <ul className={styles.posts}>
+                {posts.map((post => (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                )))}
+            </ul>
         </>
 
     )
--- creed-thoughts/src/routes.js
+++ creed-thoughts/src/routes.js
@@ -2,15 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Menu from "./components/Menu";
 import Home from "./pages/Home";
+import Rodape from "components/Footer";
+import BaseHeading from "components/BaseHeading";
+import About from "pages/About";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Home/>} />          
+        <Route path="/" element={<BaseHeading/>}>
+          <Route index element={<Home/>} />
+          <Route path="about" element={<About/>} />          
+        </Route>
         <Route path="*" element={<>Pagina nao existe</>} />        
       </Routes>
+      <Rodape/>
     </BrowserRouter>
 
   );
