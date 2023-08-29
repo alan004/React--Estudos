65be85b69ea51a4ebf28c8d8f5e242e46616ae17
alan04 <alandocarmoabilio@gmail.com>
2023-08-28 22:34:13 -0300

    feat blog posts

--- creed-thoughts/src/components/PostCard/index.jsx
+++ creed-thoughts/src/components/PostCard/index.jsx
@@ -7,7 +7,7 @@ export default function PostCard({post}){
             <div className={styles.post}>
             <img className={styles.capa} src={`/assets/posts/${post.id}/capa.png`} alt="Imagem de capa do post" />
             <h2 className={styles.titulo}>{post.titulo}</h2>
-            <Btn>Ler</Btn>
+            <Btn>Read</Btn>
             </div>
         </Link>
 
--- creed-thoughts/src/pages/404/index.jsx
+++ creed-thoughts/src/pages/404/index.jsx
@@ -9,17 +9,17 @@ export default function Erro404() {
         <>
         <div className={styles.conteudoContainer}>
             <span className={styles.texto404}>404</span>
-            <h1 className={styles.titulo}> Página não encontrada</h1>
+            <h1 className={styles.titulo}> Page not found</h1>
             <p className={styles.paragrafo}>
-            Tem certeza de que era isso que você estava procurando? 
+            Are you sure this was what you were looking for?
             </p>
-            <p className={styles.paragrafo}>Aguarde uns instantes e recarregue a página, ou volte para a página inicial.
+            <p className={styles.paragrafo}>Please wait a moment and refresh the page, or return to the home page.
             </p>
 
             <div className={styles.botaoContainer}
                 onClick={()=> navegar(-1)}
             >
-                <Btn tamanho="lg">Voltar</Btn>
+                <Btn tamanho="lg">Go back</Btn>
             </div>
             <img className={styles.imagemCachorro} src={erro404} alt="Pagina não encontrada" />
             </div>
